e8f15ae6565a7bb5c80bb59a88726a14368165ae
Леонид Мурашов <lenja-d@yandex.ru>
2021-10-03 20:38:27 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleGeneralTrain
+{
+    class ArrayAnalyser
+    {
+        public static int CountBits(int n)
+        {
+            return Convert.ToString(n, 2).ToCharArray().Select(i => i - '0').ToArray().Where(i => i == 1).Select(i => i)
+                .Sum();
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 12345;
+            Console.WriteLine(ArrayAnalyser.CountBits(a));
+        }
+    }
+}
